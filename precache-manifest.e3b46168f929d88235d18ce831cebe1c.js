8d1f8c4b21de4b3dae2c6c5095a412ec87e3e93b
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-28 19:05:39 +0545

    Updates

---
+++ precache-manifest.e3b46168f929d88235d18ce831cebe1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8b09f76a1ba82af6c9cf7db7f500cc",
+    "url": "/AddtocartReactJS/index.html"
+  },
+  {
+    "revision": "247a4b9048fc46136a00",
+    "url": "/AddtocartReactJS/static/css/main.c6f0a1fd.chunk.css"
+  },
+  {
+    "revision": "ede48aa9e0a34b32725b",
+    "url": "/AddtocartReactJS/static/js/2.2c6905e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AddtocartReactJS/static/js/2.2c6905e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247a4b9048fc46136a00",
+    "url": "/AddtocartReactJS/static/js/main.9216d448.chunk.js"
+  },
+  {
+    "revision": "2465d62d66da3981c9dd",
+    "url": "/AddtocartReactJS/static/js/runtime-main.7985abe5.js"
+  }
+]);
