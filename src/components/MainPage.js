d6251e518718d8035240b0900cc42ca4e3a44089
Soyaj Bajracharya <coolboysoyaz@gmail.com>
2020-08-31 19:06:35 +0545

    upload after final changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
 import {v4 as uuid} from 'uuid';
+import shoe from  './shoe.png'
 
 export class MainPage extends Component {
     constructor(props){
         super(props);
-        this.state={
-            items : [
+        this.state = {
+            items: [
                 {
-                    id : uuid(),
-                    title : 'Item1',
-                    quantity : 0,
-                    inCart : false,
-                    message : `Add to Cart`
+                    id: uuid(),
+                    title: 'Item1',
+                    quantity: 0,
+                    inCart: false,
+                    message: `Add to Cart`
                 },
                 {
-                    id : uuid(),
-                    title : 'Item3',
-                    quantity : 0,
-                    inCart : false,
-                    message : `Add to Cart`
+                    id: uuid(),
+                    title: 'Item2',
+                    quantity: 0,
+                    inCart: false,
+                    message: `Add to Cart`
                 },
                 {
-                    id : uuid(),
-                    title : 'Item4',
-                    quantity : 0,
-                    inCart : false,
-                    message : `Add to Cart`
+                    id: uuid(),
+                    title: 'Item3',
+                    quantity: 0,
+                    inCart: false,
+                    message: `Add to Cart`
                 },
                 {
-                    id : uuid(),
-                    title : 'Item5',
-                    quantity : 0,
-                    inCart : false,
-                    message : `Add to Cart`
+                    id: uuid(),
+                    title: 'Item4',
+                    quantity: 0,
+                    inCart: false,
+                    message: `Add to Cart`
                 }
             ],
-            cart : 0 , 
+            cart: 0 , 
         }
     }
 
-    decrement = (index) =>{  
-      const items = [].concat(this.state.items);
+    decrement = (index) => {  
+      const items = [...this.state.items];
       if(items[index].quantity >1){
           items[index].quantity -= 1
       }
@@ -49,27 +50,27 @@ export class MainPage extends Component {
           items[index].inCart = false
       }
       this.setState({
-          items : items
+          items: items
       })        
     }
 
-    increment = (index) =>{
-        const items = [].concat(this.state.items);
-        items[index].quantity += 1
+    increment = (index) => {
+      const items = [...this.state.items];
+      items[index].quantity += 1
         items[index].inCart = true
         this.setState({
-            items : items
+            items: items
         })    
     }
 
     addToCart = (index) => {
-       const items = [].concat(this.state.items)
-       if(items[index].inCart === true){
+      const items = [...this.state.items];
+      if(items[index].inCart === true){
             items[index].inCart = !items[index].inCart
             items[index].message = `Remove from Cart`
             this.setState({
-                cart : this.state.cart + 1,
-                items : items
+                cart: this.state.cart + 1,
+                items: items
             })
        }
        else if(items[index].inCart === false){
@@ -77,48 +78,53 @@ export class MainPage extends Component {
            items[index].quantity = 0
            items[index].message = `Add to Cart`
            this.setState({
-               cart : this.state.cart -1
+               cart: this.state.cart -1
            })
        }
     }
-    display = () =>{
+    display = () => {
         console.log(this.state.items)
-
     }
 
     render() {
         return (
-            <div className = "mainpage"> 
+            <div>
+                <center>
                 <button 
-                    className = "btn btn-primary"
-                    onClick = {this.display}>
-                        Cart <span className = "badge badge-light"> {this.state.cart}</span>
+                    className="btn btn-primary"
+                    onClick={this.display}
+                    id="cart">
+                    Cart <span className="badge badge-light">{this.state.cart}</span>
                 </button>
-                <div>
-                {this.state.items.map((item, index) =>(
-                    <div key = {item.id}>
-                        {item.title} -
-                        <button 
-                            className = "btn btn-secondary"
-                            disabled = {item.message === 'Remove from Cart' ? true : false}
-                            onClick ={() =>this.decrement(index)}>-
-                        </button>
-                        <span 
-                            className = "badge badge-light" 
-                            style ={{width : '2vw'}}>{item.quantity}
-                        </span>
-                        <button 
-                            className = "btn btn-secondary"
-                            disabled = {item.message === 'Remove from Cart' ? true : false}
-                            onClick = {() =>this.increment(index)}>+
-                        </button>
-                        <button 
-                            className ={item.message === 'Add to Cart' ? 'btn btn-success' : 'btn btn-danger'} 
-                            disabled = {item.quantity > 0 ? false : true} 
-                            onClick = {()=> this.addToCart(index)}>{item.message}
-                        </button>
-                    </div>
-                ))} 
+                </center>
+                <div className="mainpage"> 
+                    {this.state.items.map((item, index) => (
+                        <div key={item.id} className="item">
+                            <img src={shoe} alt="shoe" className="shoe"/>
+                            <span className="itemTitle">{item.title}</span>
+                            <div className = "indc">
+                            <button 
+                                className="btn btn-secondary"
+                                id="decrease"
+                                disabled={item.message === 'Remove from Cart' ? true : false}
+                                onClick={() => this.decrement(index)}>-
+                            </button>
+                            <span className = "itemQuantity">{item.quantity}
+                            </span>
+                            <button 
+                                id="increase"
+                                className="btn btn-secondary"
+                                disabled={item.message === 'Remove from Cart' ? true : false}
+                                onClick={() => this.increment(index)}>+
+                            </button>
+                            </div>
+                            <button
+                                className={item.message === 'Add to Cart' ? 'btn btn-success' : 'btn btn-danger'} 
+                                disabled={item.quantity > 0 ? false : true} 
+                                onClick={() => this.addToCart(index)}>{item.message}
+                            </button>
+                        </div>
+                    ))} 
                 </div>
             </div>
         )
